89df3c0ee4c5816a75420c67ba12bbed90045a9b
unknown <poojithadarshan2131@gmail.com>
2023-12-20 15:04:08 +0530

    final:

---
+++ src/main/java/manytoonea2/controller/EmpCompCont.java
@@ -0,0 +1,84 @@
+package manytoonea2.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import manytoonea2.dao.CompanyDao;
+import manytoonea2.dao.EmployeeDao;
+import manytoonea2.dto.Company;
+import manytoonea2.dto.Employee;
+
+public class EmpCompCont {
+public static void main(String[] args) {
+//	Company company=new Company();
+//	company.setId(2);
+//	company.setName("Amazon");
+//	company.setGst("ammajaaan@123");
+//	Employee employee1=new Employee();
+//	employee1.setId(3);
+//    employee1.setName("sandhya");
+//    employee1.setAddress("Bangalore");
+//    employee1.setCompany(company);
+//    
+//    Employee employee2=new Employee();
+//    employee2.setId(4);
+//    employee2.setName("shraddha");
+//    employee2.setAddress("Maharashtra");
+//    employee2.setCompany(company);
+//   
+//    CompanyDao companyDao=new CompanyDao();
+//    companyDao.saveCompany(company);
+//    
+//    EmployeeDao employeeDao=new EmployeeDao();
+////    employeeDao.saveEmployee(employee1);
+////    employeeDao.saveEmployee(employee2);  
+//    List<Employee> employees=new ArrayList<Employee>();
+//    employees.add(employee1);
+//    employees.add(employee2);
+//    employeeDao.saveEmployee(employees);
+    
+//   Update 
+//	Employee employee1=new Employee();
+//	
+//    employee1.setName("Avi");
+//    employee1.setAddress("Mysore");
+//    
+//	EmployeeDao employeeDao=new EmployeeDao();
+//	employeeDao.updateEmployee(1, employee1);
+//	
+//	Company company=new Company();
+//
+//	company.setName("Amazonnnnnn");
+//	company.setGst("ammajaaan@123");
+//	CompanyDao companyDao=new CompanyDao();
+//	
+//	companyDao.updateCompany(2, company);
+//    FIND
+	
+//	EmployeeDao employeeDao=new  EmployeeDao();
+//	employeeDao.findEmployee(1);
+//
+//	CompanyDao companyDao=new CompanyDao();
+//	companyDao.findCompany(1);
+	
+	
+//	Delete
+	CompanyDao companyDao=new CompanyDao();
+	companyDao.deleteCompany(1);
+	
+//    EmployeeDao employeeDao=new EmployeeDao();
+//    employeeDao.deleteEmployee(2);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+}
--- src/main/java/manytoonea2/dao/EmployeeDao.java
+++ src/main/java/manytoonea2/dao/EmployeeDao.java
@@ -1,5 +1,7 @@
 package manytoonea2.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
@@ -21,6 +23,28 @@ public class EmployeeDao {
 		entityTransaction.commit();
 	}
 	
+	public void saveEmployee(List<Employee> employees) {
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		entityTransaction.begin();
+		for(Employee employee:employees) {
+			entityManager.persist(employee);
+		}
+		entityTransaction.commit();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public void updateEmployee(int id,Employee employee) {
 		EntityManager entityManager=getEntityManager();
@@ -39,8 +63,30 @@ public class EmployeeDao {
 		}
 	}
 	
-	
-	
+	public void deleteEmployee(int id) {
+		EntityManager entityManager=getEntityManager();
+		Employee dbEmployee=entityManager.find(Employee.class, id);
+		if(dbEmployee!=null) {
+//			id is present them i can update the data
+			EntityTransaction entityTransaction=entityManager.getTransaction();
+			entityTransaction.begin();
+			entityManager.remove(dbEmployee);
+			
+			entityTransaction.commit();
+		}else {
+			System.out.println("Sorry Emp id is not present");
+		}
+	}
+	public void findEmployee(int id) {
+		EntityManager entityManager=getEntityManager();
+		Employee dbEmployee=entityManager.find(Employee.class, id);
+		if(dbEmployee!=null) {
+//			id is present them i can update the data
+			System.out.println(dbEmployee);
+		}else {
+			System.out.println("Sorry Emp id is not present");
+		}
+	}
 	
 	
 }
